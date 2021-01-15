f98687e8741005cc5e00e39f749837d07d9cb978
Srijan2905 <76158121+Srijan2905@users.noreply.github.com>
2021-01-15 17:07:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  car =createSprite(200,200,50,50);
+  car.velocityX = 4;
+  wall = createSprite(1000,200,20,80);
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  collide(car,wall);
+  drawSprites();
+}
+
+
